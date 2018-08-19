c34262428b638b5d61acb33b0b0cc58304380ac2
johnroge <36515818+johnroge@users.noreply.github.com>
2018-08-19 16:59:49 -0700

    dups.py - rough framework
    Adding dups.py with a rough framework of the major functions and logical flow

---
+++ dups.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+putting together a rough framework to find duplicate files
+v0.1 August 2018
+"""
+
+def main():
+    # show user instructions
+    # get user input of two folders to check
+    # allow user to request sub-directories for either/ both folders
+    # first look for common file sizes for a given file
+    # from the resulting set of files that have the same size,
+    # check for a common file hash
+    # print out list of duplicate files for the end user
+
+
+def get_user_input():
+    # need syntax for adding parameters
+    pass
+
+
+def check_file_size(file01, file02):
+    # use list comprehension and generator here
+    pass
+
+
+def check_file_hash(file01, file02):
+    # use list comprehension and generator here
+    pass
+
+
+if __name__ == '__main__':
+    main()
