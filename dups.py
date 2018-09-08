f935c4992ca7b44c9ff1822556a28e4b127bb9a6
johnroge <36515818+johnroge@users.noreply.github.com>
2018-09-08 15:07:24 -0700

    working version of dups.py uploaded

--- dups.py
+++ dups.py
@@ -1,38 +1,61 @@
 #!/usr/bin/env python3
 """
 putting together a rough framework to find duplicate files
-v0.1 August 2018
+v0.2 September 2018
 """
-import optparse
+import os
 
 
+# todo: take list of files that have equal size and compare their hash
 def main():
-    # show user instructions
-    # get user input of two folders to check
-    # allow user to request sub-directories for either/ both folders
-    # first look for common file sizes for a given file
-    # from the resulting set of files that have the same size,
-    # check for a common file hash
-    # print out list of duplicate files for the end user
+    '''
+    Get user input for two folders and then search for duplicate files
+    based on file size; next will implement a hash check
+    :return: Return a list of duplicate files
+    '''
+    folder_one = get_folder()
+    folder_two = get_folder()
+    duplicates = []
+    files_1 = get_file_path(folder_one)
+    files_2 = get_file_path(folder_two)
+    for file in files_1:
+        for item in files_2:
+            if os.path.getsize(file) == os.path.getsize(item):
+                duplicates.append(file)
+                duplicates.append(item)
+            else:
+                continue
+
+    print(duplicates)
 
 
-def get_user_input():
-    # sample code for getting input and arguments
-    parser = optparse.OptionParser()
-    parser.add_option('-s', '--subdirectories',
-                      help='include subdirectories of the folder')
-    (options, arguments) = parser.parse_args()
-    return options
+def get_file_path(directory):
+    '''
+    :param directory: folder to iterate
+    :return: return full path of each file in folder
+    '''
+    file_paths = []
+
+    for root, directories, files in os.walk(directory):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            file_paths.append(file_path)
+
+    return file_paths
 
 
-def check_file_size(file01, file02):
-    # use list comprehension and generator here
-    pass
+def get_folder():
+    '''
+    :return: Return absolute path of a chosen folder
+    '''
+    folder = input('Folder to search: ')
+    if not folder or not folder.strip():
+        return None
 
+    if not os.path.isdir(folder):
+        return None
 
-def check_file_hash(file01, file02):
-    # use list comprehension and generator here
-    pass
+    return os.path.abspath(folder)
 
 
 if __name__ == '__main__':
