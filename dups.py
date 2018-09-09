1bd85e7a681188a8f65d41b8e567b70a122f8205
johnroge <36515818+johnroge@users.noreply.github.com>
2018-09-08 17:35:54 -0700

    minor update to dups.py

--- dups.py
+++ dups.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python3
 """
-putting together a rough framework to find duplicate files
+Find duplicate files between two given folders first based on file size
+and then based on md5 hash of each file.
+Using file size first to increase speed when searching very large folders.
 v0.2 September 2018
 """
 import os
+import hashlib
 
 
-# todo: take list of files that have equal size and compare their hash
 def main():
     '''
-    Get user input for two folders and then search for duplicate files
-    based on file size; next will implement a hash check
     :return: Return a list of duplicate files
     '''
     folder_one = get_folder()
     folder_two = get_folder()
-    duplicates = []
+    list_one = []
+    list_two = []
+    final_list = []
     files_1 = get_file_path(folder_one)
     files_2 = get_file_path(folder_two)
     for file in files_1:
         for item in files_2:
             if os.path.getsize(file) == os.path.getsize(item):
-                duplicates.append(file)
-                duplicates.append(item)
+                list_one.append(file)
+                list_two.append(item)
             else:
                 continue
 
-    print(duplicates)
+    for file in list_one:
+        for item in list_two:
+            if get_hash(file) == get_hash(item):
+                final_list.append(file)
+                final_list.append(item)
+
+    print(final_list)
+
+
+def get_hash(file):
+    hash = hashlib.md5()
+    with open(file, 'rb') as afile:
+        buf = afile.read()
+        hash.update(buf)
+
+    return hash.hexdigest()
 
 
 def get_file_path(directory):
@@ -44,6 +61,7 @@ def get_file_path(directory):
     return file_paths
 
 
+# todo: sanitize user input, give syntax
 def get_folder():
     '''
     :return: Return absolute path of a chosen folder
