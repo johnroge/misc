d26bea2db188d0c170744b42d6dbbd4e0198b551
John Rogers <johnroge@outlook.com>
2019-07-13 16:31:49 -0700

    updates to text_game & characters

--- game_characters.py
+++ game_characters.py
@@ -1,10 +1,40 @@
 # character classes for text_game.py
+import random
 
 
 class Wizard:
-    pass
+    def __init__(self, name, level, health):
+        self.name = name
+        self.level = level
+        self.health = health
+
+    def attack(self, creature):
+        print(f'The Wizard {self.name} attacks the {creature.name}!')
+
+        my_roll = random.randint(1, 10) * self.level
+        creature_roll = random.randint(1, 10) * creature.level
+
+        print(f'You roll a {my_roll} versus the {creature.name} rolling'
+              f' a {creature_roll}.')
+
+        if my_roll >= creature_roll:
+            print(f'You have killed the {creature.name}!')
+            return True
+        else:
+            print(f'Sadly you have been defeated by the {creature.name}..')
 
 
 class Creature:
-    pass
+    def __init__(self, name, level, health):
+        self.name = name
+        self.level = level
+        self.health = health
+
+    def __repr__(self):
+        return "Creature {} of {} level and {} health.".format(
+            self.name,
+            self.level,
+            self.health
+        )
+
 
--- text_game.py
+++ text_game.py
@@ -5,6 +5,8 @@ useful for practicing with classes, methods and flow control
 """
 
 from game_characters import Wizard, Creature
+import random
+import time
 
 
 def main():
@@ -17,34 +19,48 @@ def main():
 
 
 def print_header():
-    print('-' * 60)
+    print()
+    print('-' * 40)
     print('         WIZARD GAME APP')
-    print('-' * 60)
+    print('-' * 40)
     print()
 
 
 def game_loop():
-
     creatures = [
-        Creature(),
-        Creature(),
-        Creature(),
-        Creature(),
-        Creature(),
+        Creature('wolf', 5, 10),
+        Creature('orc', 10, 20),
+        Creature('goblin', 10, 15),
+        Creature('dark elf', 15, 30),
+        Creature('dragon', 20, 50),
     ]
 
-    hero = Wizard()
+    hero = Wizard('Gandolf', 20, 100)
 
     while True:
 
-        cmd = input('Do you [A]ttack, [R]un away, or [L]ook around?')
+        active_creature = random.choice(creatures)
+        print(f'A level {active_creature.level} {active_creature.name} '
+              f'appears in the clearing...')
+
+        cmd = input('--> Do you [A]ttack, [R]un away, or [L]ook around?')
         cmd = cmd.lower()
         if cmd == 'a':
-            print('attack pressed')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                time.sleep(2)
+                print('Our hero runs and hides to recover his wounds..')
+                time.sleep(5)
+                print()
+                print('Our hero has returned, rested and ready for'
+                      ' adventure!')
         elif cmd == 'r':
-            print('run pressed')
+            print('Our hero runs away to fight another day!')
         elif cmd == 'l':
-            print('looks around')
+            print('The Wizard looks around and sees: ')
+            for c in creatures:
+                print(f' * A {c.name} of level {c.level}')
         else:
             print('Exiting game...')
             break
