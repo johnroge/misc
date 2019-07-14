33f8a29f4066bd789082455c5e370d4496c44613
John Rogers <johnroge@outlook.com>
2019-07-14 09:33:40 -0700

    updates to text_game & characters

--- game_characters.py
+++ game_characters.py
@@ -3,10 +3,11 @@ import random
 
 
 class Creature:
-    def __init__(self, name, level, health):
+    def __init__(self, name, level, health, defense):
         self.name = name
         self.level = level
         self.health = health
+        self.defense = defense
 
     def __repr__(self):
         return "Creature {} of {} level and {} health.".format(
@@ -15,13 +16,16 @@ class Creature:
             self.health
         )
 
+    def attack(self):
+        return random.randint(1, 10) + self.level
+
     def defensive_roll(self):
-        return random.randint(1, 10) * self.level
+        return random.randint(1, 10) + self.defense
 
 
 class LargeCreature(Creature):
-    def __init__(self, name, level, health, armor):
-        super().__init__(name, level, health)
+    def __init__(self, name, level, health, defense, armor):
+        super().__init__(name, level, health, defense)
         self.armor = armor
 
     def defensive_roll(self):
@@ -31,9 +35,14 @@ class LargeCreature(Creature):
 
 
 class MagicalCreature(Creature):
-    def __init__(self, name, level, health, magic):
-        super().__init__(name, level, health)
+    def __init__(self, name, level, health, defense, magic):
+        super().__init__(name, level, health, defense)
         self.magic = magic
+
+    def attack(self):
+        base_attack = super().attack()
+        modifier = self.magic
+        return base_attack + modifier
 
     def defensive_roll(self):
         base_roll = super().defensive_roll()
@@ -41,23 +50,17 @@ class MagicalCreature(Creature):
         return base_roll + modifier
 
 
-class Wizard(Creature):
-    def __init__(self, name, level, health, magic):
-        super().__init__(name, level, health)
-        self.magic = magic
+class Wizard(MagicalCreature):
+    def __init__(self, name, level, health, defense, magic, wisdom):
+        super().__init__(name, level, health, defense, magic)
+        self.wisdom = wisdom
 
-    def attack(self, creature):
-        print(f'The Wizard {self.name} attacks the {creature.name}!')
+    def attack(self):
+        base_attack = super().attack()
+        modifier = self.wisdom
+        return base_attack + modifier
 
-        modifier = self.magic
-        my_roll = self.defensive_roll() + modifier
-        creature_roll = creature.defensive_roll()
-
-        print(f'You roll a {my_roll} versus the {creature.name} rolling'
-              f' a {creature_roll}.')
-
-        if my_roll >= creature_roll:
-            print(f'You have killed the {creature.name}!')
-            return True
-        else:
-            print(f'Sadly you have been defeated by the {creature.name}..')
+    def defensive_roll(self):
+        base_roll = super().defensive_roll()
+        modifier = self.wisdom
+        return base_roll + modifier
--- text_game.py
+++ text_game.py
@@ -29,54 +29,45 @@ def get_creatures():
     # TODO: create a randomized set of creatures based on probability
     # TODO: randomize the creature stats within a certain range
     creatures = [
-        # name, level, health
-        Creature('wolf', 5, 10),
-        Creature('poisonous snake', 3, 5),
-        Creature('elk', 5, 10),
-        # name, level, health, magic
-        MagicalCreature('goblin', 10, 15, 10),
-        MagicalCreature('dark elf', 15, 30, 15),
-        # name, level, health, magic
-        LargeCreature('orc', 10, 20, 5),
-        LargeCreature('dragon', 20, 50, 30),
+        # name, level, health, defense
+        Creature('wolf', 5, 100, 5),
+        Creature('poisonous snake', 3, 25, 2),
+        Creature('elk', 5, 300, 5),
+        # name, level, health, defense, magic
+        MagicalCreature('goblin', 10, 350, 9, 8),
+        MagicalCreature('dark elf', 15, 450, 15, 10),
+        # name, level, health, defense, armor
+        LargeCreature('orc', 10, 500, 12, 10),
+        LargeCreature('dragon', 20, 4_000, 50, 50),
     ]
 
     return creatures
 
 
 def get_player_info():
-    # TODO: track player health
     player_name = input('What is your name, hero? ')
     return player_name.capitalize()
 
 
 def game_loop():
-
     creatures = get_creatures()
     name = get_player_info()
 
-    # name, level, health, magic
-    player_1 = Wizard(name, 20, 100, 50)
+    # name, level, health, defense, magic, wisdom
+    player_1 = Wizard(name, 20, 500, 10, 50, 10)
 
     while True:
 
         active_creature = random.choice(creatures)
+        print()
         print(f'A level {active_creature.level} {active_creature.name} '
               f'appears in the clearing...')
 
         cmd = input('--> Do you [A]ttack, [R]un away, or [L]ook around?')
         cmd = cmd.lower()
         if cmd == 'a':
-            if player_1.attack(active_creature):
-                creatures.remove(active_creature)
-            else:
-                time.sleep(2)
-                print(f'Our hero {player_1.name} runs and hides to '
-                      f'recover his wounds..')
-                time.sleep(5)
-                print()
-                print(f'Our hero {player_1.name} has returned, rested '
-                      f'and ready for another adventure!')
+            battle(player_1, active_creature)
+            # TODO: determine results and next steps
         elif cmd == 'r':
             print(f'{player_1.name} bravely runs away!')
         elif cmd == 'l':
@@ -88,8 +79,17 @@ def game_loop():
             break
 
         if not creatures:
+            print('-' * 45)
             print('The forest has been cleared of creatures!')
-            print()
+            print('-' * 45)
+
+
+def battle(player, creature):
+    # player attacks creature, misses if defensive roll is higher
+    # creature attacks player, misses if defensive roll is higher
+    # if hit then subtract damage from health
+    # remove creature from game if out of health
+    # give player chance to run away if health below 30%
 
 
 if __name__ == '__main__':
