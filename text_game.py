93f2b48dc3bce0d06b2cc8b153be17dd42c05f7c
John Rogers <johnroge@outlook.com>
2019-07-14 21:10:50 -0700

    text_game.py now v2.0

--- text_game.py
+++ text_game.py
@@ -2,7 +2,7 @@
 """
 simple text game similar to dungeons and dragons
 useful for practicing with classes, methods and flow control
-v1.5
+v2.0
 """
 
 from game_characters import Wizard, Creature, LargeCreature,\
@@ -11,10 +11,13 @@ import random
 import time
 import os
 # TODO: create random items that can be used in game play
-# TODO: create doc strings for all functions
 
 
 def main():
+    """
+    Initial flow control that initiates the game
+    :return: None
+    """
     print_header()
     player = get_player_info()
     creatures = get_creatures()
@@ -22,22 +25,35 @@ def main():
 
 
 def current_monster(creature):
+    """
+    Info regarding current active creature faced by player
+    :param creature: active creature
+    :return: None
+    """
     print()
     print(f'A level {creature.level} {creature.name} '
           f'appears in the clearing...')
 
 
 def print_header():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    """
+    Generic game header
+    :return: None
+    """
+    clear_screen()
     print()
     print('-' * 40)
     print('   WELCOME TO THE WIZARD GAME APP')
-    print('                                   v1.5')
+    print('                                   v2.0')
     print('-' * 40)
     print()
 
 
 def get_creatures():
+    """
+    Generate master list of available creatures to hunt
+    :return: List of creature instances
+    """
     # TODO: create a randomized set of creatures based on probability
     # TODO: randomize the creature stats within a certain range
     creatures = [
@@ -57,6 +73,12 @@ def get_creatures():
 
 
 def get_player_info():
+    """
+    Get user input for new player
+    :return: return an instance of Wizard as player
+    """
+    # TODO: Need to randomize some of these characteristics or provide
+    # TODO: more options to the user, maybe other character classes
     name = input('What is your name, hero? ')
     name = name.capitalize()
     player = Wizard(name,
@@ -70,6 +92,12 @@ def get_player_info():
 
 
 def game_loop(player, creatures):
+    """
+    Core game logic representing player actions and consequences
+    :param player: current player
+    :param creatures: list of available creatures
+    :return: None
+    """
     while True:
         active_creature = random.choice(creatures)
         current_monster(active_creature)
@@ -91,6 +119,10 @@ def game_loop(player, creatures):
 
 
 def user_action():
+    """
+    Current menu for player choices
+    :return: cmd
+    """
     # TODO: give player chance to rest and heal by % current health
     # TODO: add feature for loading and saving game
     cmd = input('--> Do you [A]ttack, [R]un away, [L]ook around or '
@@ -101,11 +133,22 @@ def user_action():
 
 
 def game_exit():
+    """
+    Exit game
+    :return: None
+    """
     # TODO: add save feature
     raise SystemExit
 
 
 def battle_loop(player, active_creature, creatures):
+    """
+    Flow control for player - creature fight
+    :param player: current active player
+    :param active_creature: current creature being attacked
+    :param creatures: full list of available creatures
+    :return: None
+    """
 
     win_bonus = round(active_creature.level * .5)
     while player.health >= 0 and active_creature.health >= 0:
@@ -139,12 +182,22 @@ def battle_loop(player, active_creature, creatures):
 
 
 def look_around(player, creatures):
+    """
+    View all current creatures
+    :param player: current player
+    :param creatures: all available creatures
+    :return: None
+    """
     print(f'{player.name} looks around and sees: ')
     for c in creatures:
         print(f' * A {c.name} of level {c.level}')
 
 
 def game_won():
+    """
+    Game over, player has defeated all creatures
+    :return: None
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
     print('-' * 45)
     print('Congratulations!')
@@ -154,8 +207,22 @@ def game_won():
     raise SystemExit
 
 
-def player_attack(player, active_creature):
+def clear_screen():
+    """
+    Clear the current screen of data
+    :return: None
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def player_attack(player, active_creature):
+    """
+    Flow control for player attacking a creature
+    :param player: current player
+    :param active_creature: Creature being attacked by player
+    :return: None
+    """
+    clear_screen()
     player_roll = player.attack_roll()
     creature_roll = active_creature.defensive_roll()
     print(f'Our hero has attacked the {active_creature.name}!')
@@ -170,10 +237,16 @@ def player_attack(player, active_creature):
     else:
         print(f'The wiley {active_creature.name} has dodged our attack...')
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    clear_screen()
 
 
 def creature_attack(active_creature, player):
+    """
+    Active creature attacking current player
+    :param active_creature: Current creature of melee
+    :param player: current player
+    :return: None
+    """
     creature_roll = active_creature.attack_roll()
     player_roll = player.defensive_roll()
     print(f'The {active_creature.name} fights back!')
@@ -188,7 +261,7 @@ def creature_attack(active_creature, player):
     else:
         print(f'Our hero {player.name} manages to parry the attack...')
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    clear_screen()
 
 
 if __name__ == '__main__':
