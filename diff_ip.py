a16e6ea86418973c00e35f0277551f1fcf070f83
John Rogers <johnroge@outlook.com>
2019-08-14 20:37:38 -0700

    cleaned up diff_ip.py

--- diff_ip.py
+++ diff_ip.py
@@ -1,34 +1,55 @@
 #!/usr/bin/env python3
-# practice file for checking for missing subnets
+"""
+Shamelessly stolen from stackoverflow (at least the important bits)
+Last updated: 8/14/2019
+Author: JohnR
+Version 1.0
+"""
 
 
 def main():
+    """
+    flow control
+    :return: none - writes to an output file for missing lines
+    """
     a_file = get_file_a()
     b_file = get_file_b()
     write_out = get_outfile()
 
     with open(a_file, 'r') as file1:
         with open(b_file, 'r') as file2:
-            same = set(file1).difference(file2)
+            difference = set(file1).difference(file2)
 
-    same.discard('\n')
+    difference.discard('\n')
 
     with open(write_out, 'w') as file_out:
-        for line in same:
+        for line in difference:
             file_out.write(line)
 
 
 def get_file_a():
+    """
+    first file to compare
+    :return: file a
+    """
     file_a = input('File A to check: ')
     return file_a
 
 
 def get_file_b():
+    """
+    second file for comparison
+    :return: file b
+    """
     file_b = input('File B to check: ')
     return file_b
 
 
 def get_outfile():
+    """
+    new file to write to for results of what is missing in second file
+    :return: output file
+    """
     outfile = input('File to write to: ')
     return outfile
 
