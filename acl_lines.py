b1c531fb6b6d3e9c0d1a31d1ebc6c1341ec17325
John Rogers <johroge@microsoft.com>
2019-08-20 19:20:40 -0700

    adding test file acl_lines.py

---
+++ acl_lines.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+quick script to renumber acl lines in multiple of 10
+Author: JohnR
+Last Touched: 8/20/2019
+Version .01
+"""
+
+
+def main():
+    """
+    flow control
+    :return: None - output new file with a numbered ACL list
+    """
+    pass
+
+
+def get_user_input():
+    """
+    Get user input for file to convert and file to write to
+    :return: two variables, input file and results file
+    """
+    pass
+
+
+def convert_file():
+    """
+    Should be able to do all conversion in one function, at least to start
+    :return: None - writes to a results file
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
