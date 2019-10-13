13db16821d14b4de737f3a4cdf80ff145b05565f
John Rogers <johnroge@outlook.com>
2019-10-13 15:43:09 -0700

    adding features to check_ip_subnet.py

--- check_ip_subnet.py
+++ check_ip_subnet.py
@@ -3,7 +3,8 @@
 Simple script for validating an IP address is within a given subnet
 Author: JohRoge
 Last Updated: 8/7/2019
-version: 1.0
+version: 1.1
+Notes: 1.1 creating simple user menu and looking to add new features
 """
 
 from netaddr import IPNetwork, IPAddress
@@ -11,12 +12,21 @@ from netaddr import IPNetwork, IPAddress
 
 def main():
     """
-    Main block for flow control
+    Main block for flow control; may need to create a better menu soon
     :return: Output results to screen
     """
+    # show user instructions, get their info and then show results
     instructions()
     address, subnet = get_info()
     check_ip(address, subnet)
+    get_ip_range(subnet)
+
+    # now give user chance to exit or try another ip/range
+    exit_menu = user_menu()
+    if exit_menu is True:
+        main()
+    else:
+        raise SystemExit
 
 
 def instructions():
@@ -28,6 +38,23 @@ def instructions():
     print('IPv6 address example: 2603:10a6:1200:: ')
     print('\nIPv4 subnet example: 10.3.4.0/16')
     print('IPv6 subnet example: 2603:10a6:1200::/39 ')
+
+
+def user_menu():
+    """
+    create user menu for exit and try another subnet
+    :return: True or False
+    """
+    print()
+    print()
+    print()
+    user_input = input('Please R to check another IP, any other key'
+                       ' to exit: ')
+    user_input = user_input.lower()
+    if user_input == 'r':
+        return True
+    else:
+        return False
 
 
 def get_info():
@@ -51,10 +78,23 @@ def check_ip(address, subnet):
     """
     if IPAddress(address) in IPNetwork(subnet):
         print()
-        print(f'YES: {address} is in {subnet}')
+        print(f'YES: {address} is in {subnet} subnet!')
     else:
         print()
-        print(f'Sorry, but {address} is not a part of the {subnet} subnet...')
+        print(f'*** {address} is not in the {subnet} subnet ***')
+
+
+def get_ip_range(subnet):
+    """
+    Take user subnet and list out logical range
+    :param subnet: subnet range provided by user
+    :return: n/a - show result of subnet check
+    """
+    ip_info = IPNetwork(subnet)
+
+    print()
+    print(f'Range for the {subnet} subnet is:\n {ip_info.network} - '
+          f'{ip_info.broadcast}')
 
 
 if __name__ == '__main__':
